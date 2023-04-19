105505a2951d29920ea45f4e31a56e677356e667
AleixNicolas <aleix.niol@gmail.com>
2023-04-19 13:51:44 +0200

    Added Avatars, new time settings, changed button sizes, color and timer work in progress

--- DataApp/streamlit_manager_app.py
+++ DataApp/streamlit_manager_app.py
@@ -7,25 +7,47 @@ import streamlit as st
 from loguru import logger
 import datetime, hashlib
 import pandas
+import time
+
+def countdown(minutes):
+    ph = st.empty()
+    seconds = minutes*60
+    for secs in range(seconds,0,-1):
+        mm, ss = minutes, 0
+        ph.metric("Countdown", f"{mm:02d}:{ss:02d}")
+        time.sleep(1)
+    
 
 options= ['Test', 'Ordered', 'Free']
-board_names=['1','2','3','4','5','6','7','8','9','10']
+board_names=['Abella','Cabra','Elefant','Gat','Granota','Mico','Os','Serp', 'Tortuga', 'Vaca']
 games_types = []
 games_times = []
 
 st.title('Schelling Board Game: Manager App')
 
 board_number = st.number_input('Select the number of boards',min_value=1, max_value=10,value=1, step=1)
+col1, col2 = st.columns([1,22.3])
+
 for i in range(board_number):
-    st.text('Board ' + board_names[i] + ':' + ' hyperlink')
+    col1.image('Avatares/' +  board_names[i]+'.png')
+    col2.text('Board ' + board_names[i] + ':' + ' hyperlink')
 
 games_number = st.number_input('Select the number of games',min_value=1, max_value=10,value=1, step=1)
 
+col1, col2 = st.columns([1,1])
+
+advanced_time_settings= col1.checkbox('Dou you want advanced time settings?')
+if not advanced_time_settings:
+    games_times.append(col2.number_input('Select the length of all games in minutes', min_value=5, max_value=59, value=20, step=1))    
+
+col1, col2 = st.columns([1,1])
 
 for i in range(games_number):
-   games_types.append(st.selectbox(label = 'Choose type for game nº ' + str(i+1), options = options))
-   fixed_time= st.checkbox('Do you want a fixed ammount of time for game nº ' + str(i+1) + ' ?')
-   if fixed_time is True:
-       games_times.append(st.number_input('Select the length of game nº' + str(i+1) + ' in minutes', min_value=5, max_value=30, value=5, step=1))
-   
-st.button('Stop')
+   games_types.append(col1.selectbox(label = 'Choose type for game nº ' + str(i+1), options = options))
+   if advanced_time_settings:
+       games_times.append(col2.number_input('Select the length of game nº' + str(i+1) + ' in minutes', min_value=5, max_value=59, value=20, step=1))
+
+col1, col2 = st.columns([1,1])
+if col1.button('Start', use_container_width= True):
+    countdown(games_times[0])
+col2.button('Stop', use_container_width= True)
