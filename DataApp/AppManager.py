24684904dae81db8f20139d65f347bc3e1c9a011
Dimitri Marinelli <dimitri-mar@users.noreply.github.com>
2023-06-05 21:32:15 +0200

    Included checks for creation and drop database

--- DataApp/AppManager.py
+++ DataApp/AppManager.py
@@ -23,15 +23,16 @@ class AppManager:
         """ initialize the logger """
         # setup the logger
         logger.remove()
-        self.sys_log_stdout_handler= logger.add(sys.stdout,
-                                                level=self.config.sys_log_level)
+        self.sys_log_stdout_handler = logger.add(sys.stdout,
+                                                 level=self.config.sys_log_level)
         if self.config.sys_log_save:
             output_dir = self.config.output_dir
             self.sys_log_save_handler = \
                 logger.add(f"{output_dir}/syslog.log", rotation="10 MB",
                            level=self.config.sys_log_level
                            )
-    def db_url(self)->str:
+
+    def db_url(self, only_server=False) -> str:
         """ return the database url """
         if self.config.db.db_type == "sqlite":
             if self.config.output_dir is not None:
@@ -41,34 +42,26 @@ class AppManager:
             return f"sqlite:///{sqlite_path}"
         elif self.config.db.db_type == "postgres":
             conn = self.config.db.connection
-            return f"postgresql+psycopg2://{conn['user']}:{conn['password']}@{conn['host']}:{conn['port']}/{conn['name']}"
+            server_connection = f"postgresql+psycopg2://"\
+                   f"{conn['user']}:{conn['password']}@"\
+                   f"{conn['host']}:{conn['port']}"
+            if only_server:
+                return server_connection
+            else:
+                return f"{server_connection}/{conn['name']}"
+
     #
 
-
-
-    def init_db_connection(self):
+    def init_db_connection(self) -> None:
         """ initialize the database """
         assert self.config.use_database, "use_database is False"
         db_url = self.db_url()
-
-
         logger.debug("Connecting to the database")
-        self.db_engine = create_engine(db_url )
-        try:
-            self.db_engine.connect()
-        except Exception as e:
-            logger.error(f"Error connecting to the database: {e}")
-            logger.debug(f"the configuration was: {self.config.db}")
-            sys.exit(1)
-
-        #return db_engine
-        # check if the database exists
-        # if not raise exception if exsist do nothing
+        self.db_engine = create_engine(db_url)
 
 
-
-
-
+    def check_db_exists(self, create_if_not_exists: bool = False) -> None:
+        return database_exists(self.db_engine.url)
 
 
 
--- DataApp/init_database.py
+++ DataApp/init_database.py
@@ -1,10 +1,14 @@
 """ this script create the database following  the configuration files """
 from loguru import logger
+from sqlalchemy_utils import database_exists, create_database, drop_database
+
 from  DataApp.AppManager import AppManager
+
 from DataManagement.MatchDatabase import Base
 
 
 def init_db():
+    """ initialize the database """
     logger.info("Initializing database")
     app_manager = AppManager()
     logger.debug(f"app_manager: {app_manager} is going to create initialize the database"
@@ -12,6 +16,22 @@ def init_db():
                  "with the following configurations:\n"
                 f"db_path: {app_manager.config.db}\n"
                  )
+
+    if not database_exists(app_manager.db_url()):
+        logger.info("Database does not exist, creating it")
+        create_database(app_manager.db_url())
+        logger.info("Database created")
+    else:
+        logger.info("Database already exists")
+        drop_it = input("Do you want to delete it? (y/n)")
+        if drop_it == "y":
+            if not input("Are you sure? This will delete the entire database (y/n)") == "y":
+                logger.info("Database not deleted")
+                return
+            logger.info("Deleting database")
+            drop_database(app_manager.db_url())
+            create_database(app_manager.db_url())
+            logger.info("Database deleted")
 
     app_manager.init_db_connection()
     db_engine = app_manager.db_engine
